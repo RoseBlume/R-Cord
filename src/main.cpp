1d0eb78e20d24a2e59c2c10da1046ff73403f656
RoseBlume <92864596+RoseBlume@users.noreply.github.com>
2024-04-11 16:30:03 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-#include <QtWebEngine>
+#include <QtWebView>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QtWebEngine::initialize();
+    QtWebView::initialize();
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
